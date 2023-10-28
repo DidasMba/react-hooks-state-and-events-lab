354cd83be5ce98854e4ed0c4f001ff49e5f65f4b
DidasMba <daniel.mbarushi@gmail.com>
2023-10-28 23:35:20 +0300

    add App

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -19,4 +19,36 @@ function App() {
   );
 }
 
+export default App;*/
+
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/items";
+
+function App() {
+  // Create a state variable for dark mode
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Function to handle the dark mode toggle
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  // Determine the CSS class based on the dark mode state
+  const appClass = isDarkMode ? "App dark" : "App light";
+
+  return (
+    <div className={appClass}>
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={toggleDarkMode}>Dark Mode</button>
+      </header>
+      <ShoppingList items={itemData} />
+    </div>
+  );
+}
+
 export default App;
+
+
+
