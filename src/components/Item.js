741f6f615db89c09e176ff7730a42d5795e900be
DidasMba <daniel.mbarushi@gmail.com>
2023-10-28 23:38:34 +0300

    add item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 
 function Item({ name, category }) {
   return (
@@ -10,4 +10,31 @@ function Item({ name, category }) {
   );
 }
 
+export default Item;*/
+
+import React, { useState } from "react";
+
+function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  // Function to handle adding/removing from cart
+  const toggleCart = () => {
+    setInCart(!inCart);
+  };
+
+  return (
+    <li className={inCart ? "in-cart" : ""}>
+      <span>{name}</span>
+      <span className="category">{category}</span>
+      <button className="add" onClick={toggleCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
+    </li>
+  );
+}
+
 export default Item;
+
+
+
+
